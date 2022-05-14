d83f45eb481cd2cca5dc7b41282a0f32b47acbf3
Elliot Wolk <elliot.wolk@gmail.com>
2022-05-14 15:00:34 -0400

    add-mms-to-repo: add cmdline opts to control match on att name/type/md5

--- comm-tools/add-mms-to-repo.pl
+++ comm-tools/add-mms-to-repo.pl
@@ -25,17 +25,52 @@ my $USAGE = "Usage:
 
     --dest-dir=MMS_DEST_DIR
       use MMS_DEST_DIR instead of $MMS_REPO_DIR
+
+    --att=name
+      same as: --att-filename=fuzzy   --att-filetype=ignored --att-md5=ignored
+    --att=type
+      same as: --att-filename=ignored --att-filetype=exact   --att-md5=ignored
+    --att=md5
+      same as: --att-filename=ignored --att-filetype=ignored --att-md5=exact
+      (these are the defaults)
+
+    --att-filename=MATCH_RULE
+      control how att filenames are used to match
+      MATCH_RULE
+        exact:    must match exactly
+        fuzzy:    must match, but prefixes are removed and case is ignored
+        ignored:  ignored (this is the default)
+
+    --att-filetype=MATCH_RULE
+      control how att filetypes are used to match
+      filetypes are determined by the actual file contents, with File::Type perl module
+      MATCH_RULE
+        exact:    must match exactly
+        ignored:  ignored (this is the default)
+
+    --att-md5=MATCH_RULE
+      control how att file content MD5 sums are used to match
+      MATCH_RULE
+        exact:    must match exactly (this is the default)
+        ignored:  ignored
 ";
 
 sub parseMMSDir($);
-sub getMMSKey($);
+sub getMMSKey($@);
 sub getAttFileInfo($$);
 sub filetype($);
 sub md5($);
 sub run(@);
 
+my $MATCH_RULE_EXACT = "exact";
+my $MATCH_RULE_FUZZY = "fuzzy";
+my $MATCH_RULE_IGNORED = "ignored";
+
 sub main(@){
   my $simulate = 0;
+  my $attFileName = "ignored";
+  my $attFileType = "ignored";
+  my $attMD5 = "exact";
 
   my $srcDir;
   my $destDir = $MMS_REPO_DIR;
@@ -46,6 +81,24 @@ sub main(@){
       exit 0;
     }elsif($arg =~ /^-n|-s|--simulate|--no-act|--dry-run$/){
       $simulate = 1;
+    }elsif($arg =~ /^--att=name$/){
+      $attFileName = $MATCH_RULE_FUZZY;
+      $attFileType = $MATCH_RULE_IGNORED;
+      $attMD5 =      $MATCH_RULE_IGNORED;
+    }elsif($arg =~ /^--att=type$/){
+      $attFileName = $MATCH_RULE_IGNORED;
+      $attFileType = $MATCH_RULE_EXACT;
+      $attMD5 =      $MATCH_RULE_IGNORED;
+    }elsif($arg =~ /^--att=md5$/){
+      $attFileName = $MATCH_RULE_IGNORED;
+      $attFileType = $MATCH_RULE_IGNORED;
+      $attMD5 =      $MATCH_RULE_EXACT;
+    }elsif($arg =~ /^--att-filename=($MATCH_RULE_EXACT|$MATCH_RULE_FUZZY|$MATCH_RULE_IGNORED)$/){
+      $attFileName = $1;
+    }elsif($arg =~ /^--att-filetype=($MATCH_RULE_EXACT|$MATCH_RULE_IGNORED)$/){
+      $attFileType = $1;
+    }elsif($arg =~ /^--att-md5=($MATCH_RULE_EXACT|$MATCH_RULE_IGNORED)$/){
+      $attMD5 = $1;
     }elsif($arg =~ /^--dest-dir=(.+)$/){
       $destDir = $1;
       die "ERROR: $destDir is not a directory\n" if not -d $destDir;
@@ -59,13 +112,29 @@ sub main(@){
 
   die "$USAGE\nERROR: missing MMS_SRC_DIR\n" if not defined $srcDir;
 
+  my @mmsKeyFields;
+  push @mmsKeyFields, "date";
+  push @mmsKeyFields, "from";
+  push @mmsKeyFields, "to";
+  push @mmsKeyFields, "dir";
+  push @mmsKeyFields, "dateSent";
+  push @mmsKeyFields, "subject";
+  push @mmsKeyFields, "body";
+
+  push @mmsKeyFields, "attFileName"           if $attFileName eq $MATCH_RULE_EXACT;
+  push @mmsKeyFields, "attUnprefixedFilename" if $attFileName eq $MATCH_RULE_FUZZY;
+
+  push @mmsKeyFields, "attFileType"           if $attFileType eq $MATCH_RULE_EXACT;
+
+  push @mmsKeyFields, "attMD5"                if $attMD5 eq $MATCH_RULE_EXACT;
+
   my @srcMsgDirs = glob "$srcDir/*/";
   my @destMsgDirs = glob "$destDir/*/";
 
   my %srcInfoByKey;
   for my $srcMsgDir(@srcMsgDirs){
     my $mmsInfo = parseMMSDir($srcMsgDir);
-    my $key = getMMSKey($mmsInfo);
+    my $key = getMMSKey($mmsInfo, @mmsKeyFields);
     if(defined $srcInfoByKey{$key}){
       die "ERROR: duplicate src mms key $$mmsInfo{date}\n";
     }else{
@@ -76,7 +145,7 @@ sub main(@){
   my %destInfoByKey;
   for my $destMsgDir(@destMsgDirs){
     my $mmsInfo = parseMMSDir($destMsgDir);
-    my $key = getMMSKey($mmsInfo);
+    my $key = getMMSKey($mmsInfo, @mmsKeyFields);
     if(defined $destInfoByKey{$key}){
       #print STDERR "WARNING: duplicate dest mms key $$mmsInfo{date}\n";
     }else{
@@ -141,21 +210,29 @@ sub parseMMSDir($){
   return $info;
 }
 
-sub getMMSKey($){
-  my ($mmsInfo) = @_;
+sub getMMSKey($@){
+  my ($mmsInfo, @mmsKeyFields) = @_;
   my $key = '';
   my $from = $$mmsInfo{from};
-  $from = "None" if $from eq "";
-  $key .= $from . "\n";
-  $key .= join("-", @{$$mmsInfo{to}}) . "\n";
-  $key .= $$mmsInfo{dir} . "\n";
-  $key .= $$mmsInfo{date} . "\n";
-  $key .= $$mmsInfo{dateSent} . "\n";
-  $key .= $$mmsInfo{subject} . "\n";
-  $key .= $$mmsInfo{body} . "\n";
-  for my $att(@{$$mmsInfo{att}}){
-#    $key .= $$att{unprefixedFileName} . "\n";
-    $key .= $$att{md5} . "\n";
+  $from = "" if $from =~ /^None$/i;
+
+  my %okFields = map {$_ => 1} @mmsKeyFields;
+
+  $key .= $$mmsInfo{date} . "\n"               if defined $okFields{date};
+  $key .= $from . "\n"                         if defined $okFields{from};
+  $key .= join("-", @{$$mmsInfo{to}}) . "\n"   if defined $okFields{to};
+  $key .= $$mmsInfo{dir} . "\n"                if defined $okFields{dir};
+  $key .= $$mmsInfo{dateSent} . "\n"           if defined $okFields{dateSent};
+  $key .= $$mmsInfo{subject} . "\n"            if defined $okFields{subject};
+  $key .= $$mmsInfo{body} . "\n"               if defined $okFields{body};
+
+  my @atts = @{$$mmsInfo{att}};
+  for my $att(@atts){
+    $key .= $$att{unprefixedFileName} . "\n"   if defined $okFields{attUnprefixedFilename};
+    $key .= $$att{attFileName} . "\n"          if defined $okFields{attFileName};
+    $key .= $$att{attFileType} . "\n"          if defined $okFields{attFileType};
+    $key .= $$att{size} . "\n"                 if defined $okFields{attSize};
+    $key .= $$att{md5} . "\n"                  if defined $okFields{attMD5};
   }
   return $key;
 }
